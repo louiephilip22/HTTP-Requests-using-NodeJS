b2044c72769da57d788f10ca640cc8b69d7e4e93
Philip Louie Templonuevo <philiplouietemplonuevo@yahoo.ca>
2018-02-19 22:58:53 +0000

    Modified all the steps to show th correct output

--- printingInChunks.js
+++ printingInChunks.js
@@ -1,7 +1,7 @@
 var https = require('https');
 
 function getAndPrintHTMLChunks () {
-  //var https = require('https');
+
   var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
@@ -10,10 +10,13 @@ function getAndPrintHTMLChunks () {
   https.get(requestOptions, function (response) {
     response.on('data', function(chunk) {
       console.log(chunk + '\n')
-      //console.log('[-- CHUNK OF LENGTH ' + chunk + ' --]');
-      //console.log(chunk.toString());
     })
   });
 }
 
 getAndPrintHTMLChunks();
+
+
+
+
+
--- printingWithParameters.js
+++ printingWithParameters.js
@@ -1,23 +1,17 @@
 var https = require('https');
 
-
 function getAndPrintHTML (options) {
-  var myChunk = [];
-  //var requestOptions = {};
-  //var options = requestOptions;
 
   https.get(options, function (response) {
     response.on('data', function(chunk) {
-      myChunk.push(chunk);
-      console.log(myChunk);
+      console.log(chunk + '\n')
     })
   });
 }
 
 var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step1.html'
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
 };
-
 
 getAndPrintHTML(requestOptions);
